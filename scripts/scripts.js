231811e811eb5f916c250c71c7c8a6b10834ee2d
Tim Rieder <trieder@Tims-MacBook-Pro.local>
2025-07-09 15:58:17 +0200

    added timeout delay of 3 seconds to startup to get a bad cwv score

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,43 +1,48 @@
 import {
-  buildBlock,
-  loadHeader,
-  loadFooter,
-  decorateButtons,
-  decorateIcons,
-  decorateSections,
-  decorateBlocks,
-  decorateTemplateAndTheme,
-  waitForFirstImage,
-  loadSection,
-  loadSections,
-  loadCSS,
-} from './aem.js';
+    buildBlock,
+    loadHeader,
+    loadFooter,
+    decorateButtons,
+    decorateIcons,
+    decorateSections,
+    decorateBlocks,
+    decorateTemplateAndTheme,
+    waitForFirstImage,
+    loadSection,
+    loadSections,
+    loadCSS,
+} from "./aem.js";
 
 /**
  * Builds hero block and prepends to main in a new section.
  * @param {Element} main The container element
  */
 function buildHeroBlock(main) {
-  const h1 = main.querySelector('h1');
-  const picture = main.querySelector('picture');
-  // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
-    main.prepend(section);
-  }
+    const h1 = main.querySelector("h1");
+    const picture = main.querySelector("picture");
+    // eslint-disable-next-line no-bitwise
+    if (
+        h1 &&
+        picture &&
+        h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING
+    ) {
+        const section = document.createElement("div");
+        section.append(buildBlock("hero", { elems: [picture, h1] }));
+        main.prepend(section);
+    }
 }
 
 /**
  * load fonts.css and set a session storage flag
  */
 async function loadFonts() {
-  await loadCSS(`${window.hlx.codeBasePath}/styles/fonts.css`);
-  try {
-    if (!window.location.hostname.includes('localhost')) sessionStorage.setItem('fonts-loaded', 'true');
-  } catch (e) {
-    // do nothing
-  }
+    await loadCSS(`${window.hlx.codeBasePath}/styles/fonts.css`);
+    try {
+        if (!window.location.hostname.includes("localhost"))
+            sessionStorage.setItem("fonts-loaded", "true");
+    } catch (e) {
+        // do nothing
+    }
 }
 
 /**
@@ -45,12 +50,12 @@ async function loadFonts() {
  * @param {Element} main The container element
  */
 function buildAutoBlocks(main) {
-  try {
-    buildHeroBlock(main);
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error('Auto Blocking failed', error);
-  }
+    try {
+        buildHeroBlock(main);
+    } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error("Auto Blocking failed", error);
+    }
 }
 
 /**
@@ -59,12 +64,12 @@ function buildAutoBlocks(main) {
  */
 // eslint-disable-next-line import/prefer-default-export
 export function decorateMain(main) {
-  // hopefully forward compatible button decoration
-  decorateButtons(main);
-  decorateIcons(main);
-  buildAutoBlocks(main);
-  decorateSections(main);
-  decorateBlocks(main);
+    // hopefully forward compatible button decoration
+    decorateButtons(main);
+    decorateIcons(main);
+    buildAutoBlocks(main);
+    decorateSections(main);
+    decorateBlocks(main);
 }
 
 /**
@@ -72,23 +77,26 @@ export function decorateMain(main) {
  * @param {Element} doc The container element
  */
 async function loadEager(doc) {
-  document.documentElement.lang = 'en';
-  decorateTemplateAndTheme();
-  const main = doc.querySelector('main');
-  if (main) {
-    decorateMain(main);
-    document.body.classList.add('appear');
-    await loadSection(main.querySelector('.section'), waitForFirstImage);
-  }
-
-  try {
-    /* if desktop (proxy for fast connection) or fonts already loaded, load fonts.css */
-    if (window.innerWidth >= 900 || sessionStorage.getItem('fonts-loaded')) {
-      loadFonts();
+    document.documentElement.lang = "en";
+    decorateTemplateAndTheme();
+    const main = doc.querySelector("main");
+    if (main) {
+        decorateMain(main);
+        document.body.classList.add("appear");
+        await loadSection(main.querySelector(".section"), waitForFirstImage);
     }
-  } catch (e) {
-    // do nothing
-  }
+
+    try {
+        /* if desktop (proxy for fast connection) or fonts already loaded, load fonts.css */
+        if (
+            window.innerWidth >= 900 ||
+            sessionStorage.getItem("fonts-loaded")
+        ) {
+            loadFonts();
+        }
+    } catch (e) {
+        // do nothing
+    }
 }
 
 /**
@@ -96,18 +104,18 @@ async function loadEager(doc) {
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
-  const main = doc.querySelector('main');
-  await loadSections(main);
+    const main = doc.querySelector("main");
+    await loadSections(main);
 
-  const { hash } = window.location;
-  const element = hash ? doc.getElementById(hash.substring(1)) : false;
-  if (hash && element) element.scrollIntoView();
+    const { hash } = window.location;
+    const element = hash ? doc.getElementById(hash.substring(1)) : false;
+    if (hash && element) element.scrollIntoView();
 
-  loadHeader(doc.querySelector('header'));
-  loadFooter(doc.querySelector('footer'));
+    loadHeader(doc.querySelector("header"));
+    loadFooter(doc.querySelector("footer"));
 
-  loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
-  loadFonts();
+    loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
+    loadFonts();
 }
 
 /**
@@ -115,15 +123,18 @@ async function loadLazy(doc) {
  * without impacting the user experience.
  */
 function loadDelayed() {
-  // eslint-disable-next-line import/no-cycle
-  window.setTimeout(() => import('./delayed.js'), 3000);
-  // load anything that can be postponed to the latest here
+    // eslint-disable-next-line import/no-cycle
+    window.setTimeout(() => import("./delayed.js"), 3000);
+    // load anything that can be postponed to the latest here
 }
 
 async function loadPage() {
-  await loadEager(document);
-  await loadLazy(document);
-  loadDelayed();
+    await new Promise((resolve) => {
+        setTimeout(resolve, 3000);
+    });
+    await loadEager(document);
+    await loadLazy(document);
+    loadDelayed();
 }
 
 loadPage();
